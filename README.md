69a7418c2233c52c6c1b247b9363ce69203789d6
Seyfullah Davulcu <seyfullah.davulcu@stud.uni-due.de>
2025-01-14 00:31:58 +0100

    Aufgabenübersicht aktuallisiert Readme Datei aktualisiert Code Formatting

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
+This is the testing environment for my Master Thesis.
+The goal is to find out whether an alternative syntax of SQL might
+aid programmers in finding code errors more easily.
+As part of my empirical study, test subjects will be presented with faulty SQL-Code where the linenumber of the error
+has to be found.
+The needed error localization time will be measured and analyzed.
+
+The test can be run in the browser.
+
+HTML-Preview link:
+https://htmlpreview.github.io/?https://raw.githubusercontent.com/MasterSeyf/SQL-Code-Evaluator/master/dist/index.html
+
+# Syntaxvariants
+
+- postfix alias (standard) or prefix alias (alternative)
+- postfix joinconditions (standard) or infix joinconditions (alternative)
+
 # Features
 
+- Insgesamt 4 unterschiedliche Syntaxvarianten für SQL-Code
+- Tutorial mit 8 Aufgaben (2 je Syntaxvariante)
+- Hauptexperiement mit 96 Aufgaben (24 je Sxntaxvariante)
+- Pausenübersicht nach jeder Aufgabe mit Messstopp
+- Download einer CSV-Datei am Ende
+
+# Done
+
+- Übersetzung aller Java-Dateien
 - Initiale Texteingabe von Name + Seed
 - Bunter text im Codeviewer (+ Einrückung!)
-- Texteingabe unten beim Codeviewer
-- Erfassung von Strg+Enter im Codeviewer
-- Übernahme von Tutorial 1 & 2
-- Übersetzung aller Java-Dateien
-- Download der CSV-Datei
-- StepForward
-- Es wird Pausiert
-- Download am Ende
-- Download durch ALT+ENTER
+- Texteingabefeld unten beim Codeviewer
+- Tutorial mit 2 Level an Schwierigkeiten
+- StepForward steppt vorwärts
+- Der Test kann durch HTML-Prewview im Browser durchgeführt werden
 - Style
 - More Style
-
-HTML Preview link:
-https://htmlpreview.github.io/?https://raw.githubusercontent.com/MasterSeyf/SQL-Code-Evaluator/master/dist/index.html
--- src/walloftext/Introduction1.ts
+++ src/walloftext/Introduction1.ts
@@ -7,17 +7,19 @@ const einleitung: string =
   'Zunächst werden die hier vorkommenden verschiedenen Variationen erläutert:';
 const änderung1: string =
   '\n\n' +
-  'Erste Änderung (Postfix / Prefix von Umbenennungen)\n' +
+  '____Erste Änderung (Postfix / Prefix von Umbenennungen)____\n' +
   'Standardmäßig befindet sich der Alias für die Umbenennung nach der umzubenennenden Tabelle bzw. Subquery. \n' +
   'Dies hat beispielsweise die folgende Form:';
 const code1: string =
   '\n' +
+  '____Postfix Alias:____\n' +
   'SELECT *\n' +
   'FROM   Tabelle AS Alias;\n' +
   'bzw. \n' +
   'SELECT * \n' +
   'FROM   (Subquery) AS Alias;\n' +
-  'Diese Reihenfolge wird hier bei manchen Aufgaben umgedreht und schaut wie folgt aus:\n' +
+  'Diese Syntax wird hier bei manchen Aufgaben abgeändert und schaut wie folgt aus:\n' +
+  '____Prefix Alias:____\n' +
   'SELECT *\n' +
   'FROM   Alias AS Tabelle;\n' +
   'bzw. \n' +
@@ -25,40 +27,39 @@ const code1: string =
   'FROM   Alias AS (Subquery);';
 const änderung2: string =
   '\n\n' +
-  'Zweite Änderung (Postfix / Infix / Prefix von Verbundsbedingungen)\n' +
+  '____Zweite Änderung (Postfix / Infix von Verbundsbedingungen)____\n' +
   'Standardmäßig befindet sich der Verbundsoperator zwischen den zu verknüpfenden Tabellen während die Verbundsbedingung nach der zweiten Tabelle definiert wird. \n' +
   'Dies hat beispielsweise die folgende Form:';
 const code2: string =
   '\n' +
-  'Postfix Verbundsbedingung:\n' +
+  '____Postfix Verbundsbedingung:____\n' +
   'SELECT *\n' +
   'FROM   Tabelle1\n' +
   '       JOIN\n' +
   '       Tabelle2\n' +
   '       ON (Tabelle1.attribut = Tabelle2.attribut); \n' +
-  'Diese Reihenfolge wird hier bei manchen Aufgaben auf zwei Weisen abgeändert und schaut wie folgt aus:\n' +
-  'Infix Verbundsbedingung:\n' +
+  'Diese Syntax wird hier bei manchen Aufgaben abgeändert und schaut wie folgt aus:\n' +
+  '____Prefix Verbundsbedingung:____\n' +
   'SELECT *\n' +
-  'FROM   Tabelle1\n' +
-  '       ⋈ (Tabelle1.attribut = Tabelle2.attribut) \n' +
-  '       Tabelle2;\n' +
-  'Prefix Verbundsbedingung:\nSELECT *\nFROM   ⋈ (Tabelle1.attribut = Tabelle2.attribut)\n       Tabelle1\n       Tabelle2;';
+  'FROM   ⋈ (Tabelle1.attribut = Tabelle2.attribut)\n' +
+  '       Tabelle1\n' +
+  '       Tabelle2;';
 const ausleitung: string =
-  '\n\n' +
-  'Diese 2 Variationen bezüglich der Umbenennung als auch die 3 Variationen bezüglich des Verbundsoperators können in jeder Kombination auftreten. \n' +
-  'Daraus resultieren 6 mögliche Aufgabentypen, die Ihnen begegnen können.' +
-  '\nIhre Aufgabe wird es sein, die Zeile in der sich ein Syntaxfehler hinsichtlich einer Verbunds- oder Selektionsbedingung befindet zu erkennen und aufzuschreiben.' +
-  '\nDamit Sie sich mit den verschiedenen Kombinationsmöglichkeiten vertraut machen, werden im Folgenden Übungsaufgaben präsentiert, bei denen Sie Feedback zu Ihren Lösungen erhalten.' +
+  '\n' +
+  '\nDiese 2 Variationen bezüglich der Umbenennung als auch die 2 Variationen bezüglich des Verbundsoperators können in jeder Kombination auftreten.' +
+  '\nDaraus resultieren 4 mögliche Aufgabentypen, die Ihnen begegnen können.' +
+  '\nWenn Ihnen SQL Code präsentiert wird befindet sich immer genau 1 Syntaxfehler entweder in einer Verbunds- oder Selektionsbedingung.' +
+  '\nIhre Aufgabe wird es sein, die Zeile in der sich dieser Syntaxfehler befindet zu erkennen, aufzuschreiben und abzuschicken.' +
+  '\nDamit Sie sich mit den verschiedenen Kombinationsmöglichkeiten vertraut machen, werden im Folgenden zunächst Übungsaufgaben präsentiert, bei denen Sie Feedback zu Ihren Lösungen erhalten.' +
   '\nBei einer richtigen Eingabe färbt sich das Abgabefeld Grün und es wird mit der nächsten Aufgabe fortgesetzt, während sich bei einer falschen Eingabe das Abgabefeld Rot färbt und Sie Ihre Eingabe korrigieren müssen, bis sie stimmt.' +
   '\nNach dem Tutorial wird es kein Feedback mehr zu den Abgaben gegeben und somit werden auch fehlerhafte Antworten zum Fortsetzen mit der nächsten Aufgabe führen.' +
-  '\nVor jeder Aufgabe wird Ihnen im Vorfeld genannt, um welche Variantenkombination es sich in der kommenden Aufgabe handeln wird. \n' +
-  'Nach jeder abgegebenen Lösung können Sie in der Zwischenübersicht eine Pause einlegen.' +
-  '\n' +
-  'Falls Sie Fragen zu den Syntaxen oder zum weiteren Ablauf haben sollten so klären Sie diese bitte jetzt mit der Aufsichtsperson. \n' +
-  'Zum Verlassen dieser Hilfsübersicht drücken Sie bitte die Tastenkombination Strg + Enter.';
+  '\nVor jeder Aufgabe wird Ihnen im Vorfeld genannt, um welche Variantenkombination es sich in der kommenden Aufgabe handeln wird.' +
+  '\nNach jeder abgegebenen Lösung können Sie in der Zwischenübersicht eine Pause einlegen.' +
+  '\nFalls Sie Fragen zu den Syntaxen oder zum weiteren Ablauf haben sollten so klären Sie diese bitte jetzt mit der Aufsichtsperson.' +
+  '\nZum Verlassen dieser Hilfsübersicht drücken Sie bitte die Tastenkombination Strg + Enter.';
 
 const Introduction1: TaskDefinition = {
-  text: einleitung + änderung1 + code1 + änderung2 + code2 + ausleitung,
+  text: einleitung + änderung1 + code1 + änderung2 + code2 + ausleitung
 };
 
 export default Introduction1;
